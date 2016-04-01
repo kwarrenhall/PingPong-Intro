9bcdd3115c8653e5d54e343d550b52abe95c1ae1
kate hall <padma@email.com>
2016-04-01 10:39:16 -0700

    add working test for replacing numbers divisible by 5 with 'pong'

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,11 @@ function pingPong(input) {
   for(i = 1; i <= input; i++) {
     if (i % 3 === 0) {
       result.push('ping');
+    } else if (i % 5 === 0) {
+      result.push('pong');
     } else {
       result.push(i);
-      }
+    }
   console.log(result);
   }
 }
