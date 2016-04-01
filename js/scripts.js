21c46b0e942f90b94dc8ed4892ba97caf1860eef
kate hall <padma@email.com>
2016-04-01 10:32:19 -0700

    add first passing test for counting up to a user number

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
-var leapYear = function(year) {
-  //Lines 2-6 are not necessary for functionality because everything that's not a leap year is returned as false. Lines are used for readability and logic
-  if (parseInt(year) === NaN) {
-    return false;
-  } else if (year === "september") {
-    return false;
-  } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+function pingPong(input) {
+  var result = [];
+  for(i = 1; i <= input; i++) {
+    result.push(i);
+    }
+  console.log(result);
+}
+
+pingPong(10);
