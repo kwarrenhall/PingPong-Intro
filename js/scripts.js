fcbbdd48acbdc1628d220a5d56c970f5e4d56178
kate hall <padma@email.com>
2016-04-01 10:46:32 -0700

    add working test for replacing numbers divisible by both 3 & 5 with 'pingpong'

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 function pingPong(input) {
   var result = [];
   for(i = 1; i <= input; i++) {
-    if (i % 3 === 0) {
-      result.push('ping');
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      result.push('pingpong');
     } else if (i % 5 === 0) {
       result.push('pong');
+    } else if (i % 3 === 0) {
+      result.push('ping');
     } else {
       result.push(i);
     }
-  console.log(result);
   }
+  console.log(result);
 }
-pingPong(10);
+pingPong(50);
